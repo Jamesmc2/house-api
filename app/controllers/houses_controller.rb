65e4fa880b4a4ad0e525dc3c0a52805d32f84657
jimmy <jamesmc21998@gmail.com>
2023-09-07 11:31:45 -0500

    add create route for houses

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -3,4 +3,15 @@ class HousesController < ApplicationController
     @houses = House.all()
     render :index
   end
+
+  def create
+    @house = House.new(
+      address: params[:address],
+      squarefeet: params[:squarefeet],
+      bedrooms: params[:bedrooms],
+      bathrooms: params[:bathrooms],
+    )
+    @house.save
+    render :show
+  end
 end
